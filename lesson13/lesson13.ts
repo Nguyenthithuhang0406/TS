95acc619a18c642ca4095ccac4fb5b74e7bf852e
Nguyenthithuhang0406 <hangthongminh04062004@gmail.com>
2024-07-17 00:21:31 +0700

    tuple type

---
+++ lesson13/lesson13.ts
@@ -0,0 +1,8 @@
+//hoat dong giong nhu array
+
+//quan trọng kiểu dữ liệu và thứ tự kiểu dữ liệu là thứ tự dữ liệu truyền vào
+let skills: [string, number] = ['hit', 2024];
+
+
+//có hoặc không (optional), tham số optional phải đặt cuối cùng
+let skills2: [string, number?] = ['hit', 2024];
