c3b62a9444d436f7ea529c79127f006e67499cfa
Nguyenthithuhang0406 <hangthongminh04062004@gmail.com>
2024-07-17 00:21:18 +0700

    array type

---
+++ lesson12/lesson12.ts
@@ -0,0 +1,12 @@
+let arr1: number[] = [1, 2, 3, 4, 5];
+
+let myarr: string[] = ["hit", "2024"];
+
+console.log("arr1: ", arr1);
+console.log("myarr: ", myarr);
+
+// Array of mixed types
+//nhieu kieu du lieu co the de tu assign cho 1 mang (khong khai bao kieu du lieu)
+let arr3: (string | number)[] = ["hit", 2024];
+
+console.log("arr3: ", arr3);
