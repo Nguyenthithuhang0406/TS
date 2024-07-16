43a7ee463e2daed0fdd2ae72e915f9460a26ba2e
Nguyenthithuhang0406 <hangthongminh04062004@gmail.com>
2024-07-17 00:20:31 +0700

    string type

---
+++ lesson9/lesson9.ts
@@ -0,0 +1,14 @@
+let name2: string = "Hit 2024";
+
+//dùng \ để bỏ qua ký tự đặc biệt
+let name3: string = "Hit và \"2024\"";
+
+//dùng ` để viết nhiều dòng
+let name4: string = `Hit và 
+"2024"`;
+
+let name1: string = 'Hit TS';
+
+console.log("Name 1: ", name1);
+console.log("Name 2: ", name2);
+console.log("Name 3: ", name3);
