f5ab4e5eecea5d919adab58517fb41cee8db8f6c
Nguyenthithuhang0406 <hangthongminh04062004@gmail.com>
2024-07-17 00:21:03 +0700

    object type

---
+++ lesson11/lesson11.ts
@@ -0,0 +1,10 @@
+let mine = {};
+
+let mine1: { name: string, age: number } = { name: "Hang", age: 20 };
+
+let person: object;
+let count: {};
+
+person = { name: "Hit" };
+
+console.log("mine1", mine1);
