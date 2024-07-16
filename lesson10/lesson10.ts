a0851fa11b2dea0ceaa7e9418a1245ae5ceab5f7
Nguyenthithuhang0406 <hangthongminh04062004@gmail.com>
2024-07-17 00:20:53 +0700

    boolean type

---
+++ lesson10/lesson10.ts
@@ -0,0 +1,11 @@
+let status1: boolean = false;
+
+let check1: boolean = true;
+
+
+//Boolean là lớp đối tượng, sẽ convert về boolean
+let hehe: boolean = Boolean("2024");
+
+console.log("status:", status1);
+console.log("check:", check1);
+console.log("hehe:", hehe);
